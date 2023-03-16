24610191a4b3d437deea01e1e1fbfd39babcce68
Alexander Rodriguez <alexrguez@gmail.com>
2023-03-16 15:18:56 +0000

    remove phone detection

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -10,28 +10,24 @@ const article3 = document.querySelector('#article3');
 
 button1.addEventListener('click', () => {
   toggleVisibility(article1);
-	
-});
+	});
 
 button2.addEventListener('click', () => {
 	toggleVisibility(article2);
 });
 
 button3.addEventListener('click', () => {
-  console.log("Pressed button 3");
 	toggleVisibility(article3);
 });
 
 function toggleVisibility(param1) {
 
   const computedStyle = getComputedStyle(param1);
-  console.log("Inside Function");
   
   if (computedStyle.display === "none" ) {
-    param1.style.display = "block";
-    console.log("If");
+    param1.style.display = "flex";
   } else {
     param1.style.display = "none";
-    console.log("Else");
   }
+  
 }
