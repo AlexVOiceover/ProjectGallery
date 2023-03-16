062bf2514e4dcb3d8c643e62f2837187bfde0781
Alexander Rodriguez <alexrguez@gmail.com>
2023-03-16 14:53:16 +0000

    Fixed toggleVisibility using getComputedStyle

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,40 +1,37 @@
 console.log("Initialising console");
 
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
 
-// document.getElementById("myBtn1").onclick = function() {toggleVisibility("artic1")};
-// document.getElementById("myBtn2").onclick = function() {toggleVisibility("artic2")};
-// document.getElementById("myBtn3").onclick = function() {toggleVisibility("artic3")};
+const article1 = document.querySelector('#article1');
+const article2 = document.querySelector('#article2');
+const article3 = document.querySelector('#article3');
 
-// document.getElementsByClassName(".dropbtn").onclick = function() {toggleVisibility('artic3')};
+button1.addEventListener('click', () => {
+  toggleVisibility(article1);
+	
+});
 
-// store in a variable what I want to manipulate
-const nombre = document.querySelector(".dropdown-content");
-console.log(nombre);
-const btn = document.querySelector(".dropbtn").onclick;
-console.log(btn);
-btn.addEventListener("click", toggleVisibility);
-// console.log(nombre);
+button2.addEventListener('click', () => {
+	toggleVisibility(article2);
+});
 
-
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-// function toggleVisibility(param1) {
-//   console.log("Inside myFunction");
-//   document.getElementById(param1).classList.toggle("show");
-// }
+button3.addEventListener('click', () => {
+  console.log("Pressed button 3");
+	toggleVisibility(article3);
+});
 
 function toggleVisibility(param1) {
-  console.log("Inside myFunction");
-  // nombre.classList.toggle("show");}
-  param1.classList.toggle("show");}
 
-// function showMore() {
-//   if (article.className == "open") {
-//     // read less
-//     article.className = "";
-//     button.innerHTML = "Show more";
-//   } else {
-//     //read more
-//     article.className = "open";
-//     button.innerHTML = "Show less";
-//   }
-// }
+  const computedStyle = getComputedStyle(param1);
+  console.log("Inside Function");
+  
+  if (computedStyle.display === "none" ) {
+    param1.style.display = "block";
+    console.log("If");
+  } else {
+    param1.style.display = "none";
+    console.log("Else");
+  }
+}
