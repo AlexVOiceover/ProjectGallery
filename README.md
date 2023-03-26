6a5ad35cbbd8f1031f24f5d1ea677a8bc99a8e55
Alexander Rodriguez <alexrguez@gmail.com>
2023-03-26 12:21:01 +0100

    Simplified the containers structure and readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ProjectGallery
-I showcase some of my previous projects
+This simple website shows two flex containers:
+
+* The first one has a row disposition for desktop mode and will change to row disposition when shown in a mobile device.
+* Each element of the first container will be also a flex container, but in this case it's class is `flex-nested-container`. These have a column disposition (no matter if it's being shown in desktop mode or in a mobile device). These flex containers contains 4 elements:
+
+    1. A `h3` title
+    2. A static image
+    3. The button that will tocle the article
+    4. The article itself that will be shown or hidden
+
+I also created three classes: `flex-nested-container-small`, `flex-nested-container-medium` and `flex-nested-container-large` to play with different sizes, even when the resulting aesthetics is not very appealing.
--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,13 +1,16 @@
 console.log("Initialising console");
 
+// Get the buttons
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
 const button3 = document.querySelector('#button3');
 
+// Get the articles
 const article1 = document.querySelector('#article1');
 const article2 = document.querySelector('#article2');
 const article3 = document.querySelector('#article3');
 
+// Add event listeners to buttons
 button1.addEventListener('click', () => {
   toggleVisibility(article1);
 });
@@ -20,12 +23,13 @@ button3.addEventListener('click', () => {
 	toggleVisibility(article3);
 });
 
-function toggleVisibility(param1) {
-  const computedStyle = getComputedStyle(param1);
+// This functions toggles the visibility of param
+function toggleVisibility(param) {
+  const computedStyle = getComputedStyle(param);
   
   if (computedStyle.display === "none" ) {
-    param1.style.display = "inline-block";
+    param.style.display = "inline-block";
   } else {
-    param1.style.display = "none";
+    param.style.display = "none";
   }
 }
